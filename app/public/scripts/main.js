d5b7473ceea629e5ba6c8d37e3a8204efec09ea7
simon0191 <simon0191@gmail.com>
2014-09-13 17:33:34 -0500

    Set login as default page

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -2,47 +2,49 @@ $(document).ready(function() {
 
   var mainRegionTemplate = _.template($('#main-region-template').html());
   var $mainRegion = $('#main-region');
-  var routes = {
-    '/register': function () {
-      $mainRegion.html(mainRegionTemplate({
-        carousel: {
-          mainText:'Create your account'
-        },
-        form: {
-          inputs: [
-            {placeholder:'Your name'},
-            {placeholder:'Your email address'},
-            {placeholder:'Your password'}
-          ],
-          submit: 'Sign up',
-          info: {
-            text: 'Got an account already?',
-            link: 'Log in'
-          }
+  var register = function() {
+    $mainRegion.html(mainRegionTemplate({
+      carousel: {
+        mainText:'Create your account'
+      },
+      form: {
+        inputs: [
+          {placeholder:'Your name'},
+          {placeholder:'Your email address'},
+          {placeholder:'Your password'}
+        ],
+        submit: 'Sign up',
+        info: {
+          text: 'Got an account already?',
+          link: 'Log in'
         }
-      }));
-    },
-    '/login': function () {
-      $mainRegion.html(mainRegionTemplate({
-        carousel: {
-          mainText:'Login'
-        },
-        form: {
-          inputs: [
-            {placeholder:'Your email address'},
-            {placeholder:'Your password'}
-          ],
-          submit: 'Login',
-          info: {
-            text: "Don't have an account?",
-            link: 'Sign up'
-          }
-        }
-      }));
-    }
+      }
+    }));
   };
+  var login = function() {
+    $mainRegion.html(mainRegionTemplate({
+      carousel: {
+        mainText:'Login'
+      },
+      form: {
+        inputs: [
+          {placeholder:'Your email address'},
+          {placeholder:'Your password'}
+        ],
+        submit: 'Login',
+        info: {
+          text: "Don't have an account?",
+          link: 'Sign up'
+        }
+      }
+    }));
+  }
 
-  var router = Router(routes).configure({
+  var router = Router({
+    '/': login,
+    '/login': login,
+    '/register': register
+  }).configure({
     html5history: true
   });
 
